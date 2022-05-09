ffab24a85f03eea1aab6fd83a60c1f17b35cf884
starunity <star@starunity.dev>
2022-05-09 15:33:53 +0800

    fix: 🐛 remove old dashboard setting

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -64,7 +64,6 @@ lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 
 -- TODO: User Config for predefined plugins
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
-lvim.builtin.dashboard.active = true
 lvim.builtin.notify.active = true
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
